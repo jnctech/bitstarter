fcc36e485dce1e958c98ea87d6406afdef07a239
Jason Cole <jc@jasoncole.info>
2013-07-11 02:26:01 +0000

    try again

--- web.js
+++ web.js
@@ -1,11 +1,11 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-var fs = require('fs')
 
 app.get('/', function(request, response) {
-   var buf = new Buffer(fs.readFilesync('index.html'), 'utf-8');
-  response.send(buf.toString());
+ var fs = require('fs')
+ var buf = new Buffer(fs.readFilesync('index.html'), 'utf-8');
+ response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
